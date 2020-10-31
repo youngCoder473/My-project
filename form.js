495a18464163c1b4b55dcc0c37b34e5fe75ec99b
youngCoder473 <lakshya.bordia@gmail.com>
2020-10-31 14:21:46 -0400

    myProject

---
+++ form.js
@@ -0,0 +1,34 @@
+class Form{
+    constructor(){
+        this.input = createInput("Name")
+        this.button = createButton("play")
+        this.title = createElement("h2")
+    }
+    hide(){
+        this.input.hide();
+        this.title.hide();
+        this.button.hide();
+    }
+    displey(){
+        this.title.html("The Killer Game");
+        this.title.position(350, 50);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'red');
+        this.input.position(550,400);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'red');
+        this.button.position(560,500);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'red');
+        
+        this.button.mousePressed(() =>{
+            hide();
+            player.name = this.input.value();
+            playerIdentity = random(1,5)
+            
+        })
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database
+var gameState
+var playerCount
+var allPlayers
+var killer 
+var civilianGroup
+var Detective
+var PlayerImage
+var playerNumber
+var player
+var killer = 1
+var detective = 5
+var civilian = [2,3,4]
+
+function preload(){
+    
+}
+
+function setup(){
+   dataBase = firebase.database();
+
+}
+
+function draw(){
+    
+}   
+
